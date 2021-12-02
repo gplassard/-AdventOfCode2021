95b742241bbb03aa11266f9f26c914942eb2164b
Gabriel Plassard <gabriel.plassard@gmail.com>
2021-12-02 21:07:12 +0100

    Day 1 / part 2

--- src/main/scala/day1/Day1.scala
+++ src/main/scala/day1/Day1.scala
@@ -3,9 +3,19 @@ package day1
 
 object Day1 {
 
-  def countIncreases(measurements: List[Int]): Int =
+  def part1(measurements: List[Int]): Int =
     if (measurements.length <= 1) 0
     else measurements.zip(measurements.tail)
       .count((a, b) => b > a)
+
+  def part2(measurements: List[Int]): Int = {
+    if (measurements.length <= 3) 0
+    else {
+      val tripletsSums = measurements.zip(measurements.tail).zip(measurements.tail.tail)
+        .map{case ((a,b), c) => a + b + c}
+
+      part1(tripletsSums)
+    }
+  }
 
 }
--- src/test/scala/day1/Day1Test.scala
+++ src/test/scala/day1/Day1Test.scala
@@ -11,31 +11,59 @@ import scala.jdk.StreamConverters._
 
 class Day1Test extends AnyWordSpec with Matchers {
 
-  "countIncreases" should {
+  "part1" should {
     "be 0 for empty measurements" in {
-      Day1.countIncreases(List.empty) should equal(0)
+      Day1.part1(List.empty) should equal(0)
     }
 
     "be 0 for single measurements" in {
-      Day1.countIncreases(List(12)) should equal(0)
+      Day1.part1(List(12)) should equal(0)
     }
 
     "be 0 for decreasing measurements" in {
-      Day1.countIncreases(List(12, 11, 10)) should equal(0)
+      Day1.part1(List(12, 11, 10)) should equal(0)
     }
 
     "count the number of increases" in {
-      Day1.countIncreases(List(10, 11, 9, 13, 12)) should equal(2)
+      Day1.part1(List(10, 11, 9, 13, 12)) should equal(2)
     }
 
     "work for the sample" in {
       val measures = Files.lines(Paths.get("src/test/resources/day1/sample.txt")).map(l => Integer.parseInt(l)).toScala(List)
-      Day1.countIncreases(measures) should equal(7)
+      Day1.part1(measures) should equal(7)
     }
 
     "work for the input" in {
       val measures = Files.lines(Paths.get("src/test/resources/day1/input.txt")).map(l => Integer.parseInt(l)).toScala(List)
-      Day1.countIncreases(measures) should equal(1298)
+      Day1.part1(measures) should equal(1298)
+    }
+  }
+
+  "part2" should {
+    "be 0 for empty measurements" in {
+      Day1.part2(List.empty) should equal(0)
+    }
+
+    "be 0 for 3 measurements" in {
+      Day1.part2(List(1, 2, 3)) should equal(0)
+    }
+
+    "be 1 for increased measurements" in {
+      Day1.part2(List(1, 2, 3 ,4)) should equal(1)
+    }
+
+    "be 0 for decreased measurements" in {
+      Day1.part2(List(1, 2, 3 , 0)) should equal(0)
+    }
+
+    "work for the sample" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day1/sample.txt")).map(l => Integer.parseInt(l)).toScala(List)
+      Day1.part2(measures) should equal(5)
+    }
+
+    "work for the input" in {
+      val measures = Files.lines(Paths.get("src/test/resources/day1/input.txt")).map(l => Integer.parseInt(l)).toScala(List)
+      Day1.part2(measures) should equal(1248)
     }
   }
 
