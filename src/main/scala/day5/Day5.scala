bdf7964d59fd3c5127a2cadfe27329919026021d
Gabriel Plassard <gabriel.plassard@gmail.com>
2021-12-05 20:13:32 +0100

    Day 5 / part 1

---
+++ src/main/scala/day5/Day5.scala
@@ -0,0 +1,49 @@
+package fr.gplassard.adventofcode2021
+package day5
+
+import scala.annotation.tailrec
+import scala.util.control.Breaks._
+
+object Day5 {
+  case class Point(x: Int, y: Int)
+
+  def collinear(a: Point, b: Point, c: Point): Boolean =
+    (b.x - a.x) * (c.y - a.y) == (c.x - a.x) * (b.y - a.y)
+
+  def within(p: Int, q: Int, r: Int): Boolean =
+    (p <= q && q <= r) || (r <= q && q <= p)
+
+  case class Line(start: Point, end: Point) {
+    def includes(point: Point): Boolean = {
+//      collinear(start, end, point) && (if (start.x != end.x) within(start.x, point.x, end.x) else within(start.y, point.y, end.y))
+      val horizontal = start.x == point.x && point.x == end.x && within(start.y, point.y, end.y)
+      val vertical = start.y == point.y && point.y == end.y && within(start.x, point.x, end.x)
+      horizontal || vertical
+    }
+  }
+
+
+  def part1(input: List[String]): Int = {
+    val lines = input.map(line => {
+      val splitted = line.split(" -> ")
+      val start = splitted(0).split(",")
+      val end = splitted(1).split(",")
+      Line(
+        Point(start(0).toInt, start(1).toInt),
+        Point(end(0).toInt, end(1).toInt),
+      )
+    })
+    val maxX = Math.max(lines.map(_.start.x).max, lines.map(_.end.x).max)
+    val maxY = Math.max(lines.map(_.start.y).max, lines.map(_.end.y).max)
+
+    val points = for {
+      x <- 0 to maxX
+      y <- 0 to maxY
+    } yield lines.count(_.includes(Point(x, y)))
+
+    val result = points.count(_ >= 2)
+    result
+  }
+
+
+}
---
+++ src/test/scala/day5/Day5Test.scala
@@ -0,0 +1,28 @@
+package fr.gplassard.adventofcode2021
+package day5
+
+import day4.Day4.Board
+
+import org.scalatest.*
+import org.scalatest.matchers.should.*
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.nio.file.{Files, Paths}
+import scala.io.Source
+import scala.jdk.StreamConverters.*
+
+class Day5Test extends AnyWordSpec with Matchers {
+
+  "part1" should {
+    "work for sample" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day5/sample.txt")).toScala(List)
+      Day5.part1(inputs) should equal(5)
+    }
+    "work for input" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day5/input.txt")).toScala(List)
+      Day5.part1(inputs) should equal(4728)
+    }
+  }
+
+
+}
