5b624799a880fa5a0dd01f65fb08602ec0593538
Gabriel Plassard <gabriel.plassard@gmail.com>
2021-12-07 13:47:52 +0100

    Day 7 / part 2

--- src/main/scala/day7/Day7.scala
+++ src/main/scala/day7/Day7.scala
@@ -10,10 +10,31 @@ object Day7 {
     positions.sorted.toList(positions.size / 2)
   }
 
+  def optimalPositionPart2(positions: List[Int]): List[Int] = {
+    val sum = positions.sum.toDouble
+    val elements = positions.length.toDouble
+    val mean = math.round(sum / elements).toInt
+    List(mean - 1, mean, mean + 1)
+  }
+
   def part1(input: List[String]): Long = {
     val positions = input.flatMap(line => line.split(",")).map(_.toInt)
     val optimal = optimalPosition(positions)
     positions.map(p => Math.abs(p - optimal)).sum
+  }
+
+  def costPart2(distance: Int): Int = {
+    (distance * (distance + 1)) / 2
+  }
+
+  def part2(input: List[String]): Long = {
+    val positions = input.flatMap(line => line.split(",")).map(_.toInt)
+    val optimals = optimalPositionPart2(positions)
+    val costs = optimals.map( maybeOptimal =>
+      positions.map(p => costPart2(Math.abs(p - maybeOptimal))).sum
+    )
+
+    costs.min
   }
 
 }
--- src/test/scala/day7/Day7Test.scala
+++ src/test/scala/day7/Day7Test.scala
@@ -61,4 +61,64 @@ class Day7Test extends AnyWordSpec with Matchers {
     }
   }
 
+
+  "optimalPositionPart2" should {
+    "work for single element" in {
+      val result = Day7.optimalPositionPart2(List(1))
+      result should equal(List(0, 1, 2))
+    }
+    "work for two elements" in {
+      // 1 => 0 + (1 + 2 + 3) = 6
+      // 2 => 1 + (1 + 2) = 4
+      // 3 => (1 + 2) + 1 = 4
+      // 4 => (1 + 2 + 3) + 0 = 6
+      val result = Day7.optimalPositionPart2(List(1, 4))
+      result should equal(List(2, 3, 4))
+    }
+    "work for three elements" in {
+      // 2 => 1 + 0 + (1 + 2 + 3 + 4) = 11
+      // 3 => (1 + 2) + 1 + (1 + 2 + 3) = 10
+      // 4 => (1 + 2 + 3) + (1 + 2) + (1 + 2) = 12
+      val result = Day7.optimalPositionPart2(List(1, 2, 6))
+      result should equal(List(2, 3, 4))
+    }
+    "work for sample" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day7/sample.txt")).toScala(List).flatMap(_.split(",")).map(_.toInt)
+
+      val result = Day7.optimalPositionPart2(inputs)
+      result should equal(List(4, 5, 6))
+    }
+    "work for input" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day7/input.txt")).toScala(List).flatMap(_.split(",")).map(_.toInt)
+      val result = Day7.optimalPositionPart2(inputs)
+      result should equal(List(465, 466, 467))
+    }
+  }
+
+  "costPart2" should {
+    "be 0 for distance 0" in {
+      Day7.costPart2(0) should equal(0)
+    }
+    "be 1 for distance 1" in {
+      Day7.costPart2(1) should equal(1)
+    }
+    "be 3 for distance 2" in {
+      Day7.costPart2(2) should equal(3)
+    }
+    "be 6 for distance 3" in {
+      Day7.costPart2(3) should equal(6)
+    }
+  }
+
+  "part2" should {
+    "work for sample" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day7/sample.txt")).toScala(List)
+      Day7.part2(inputs) should equal(168)
+    }
+    "work for input" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day7/input.txt")).toScala(List)
+      Day7.part2(inputs) should equal(97164301)
+    }
+  }
+
 }
