6faa3830dde6127c84538d3f976c899546c74f32
Gabriel Plassard <gabriel.plassard@gmail.com>
2021-12-09 00:34:07 +0100

    Day 8 / part 2

--- src/main/scala/day8/Day8.scala
+++ src/main/scala/day8/Day8.scala
@@ -5,20 +5,83 @@ import scala.annotation.tailrec
 import scala.util.control.Breaks.*
 
 object Day8 {
+  val segmentsToDigit: Map[String, Int] = Map(
+     "abcefg" -> 0,
+     "cf" -> 1,
+     "acdeg" -> 2,
+     "acdfg" -> 3,
+     "bcdf" -> 4,
+     "abdfg" -> 5,
+     "abdefg" -> 6,
+     "acf" -> 7,
+     "abcdefg" -> 8,
+     "abcdfg" -> 9,
+  )
+
+  val signatureMap: Map[Signature, Char] = ('a' to 'g').map(c => (computeSignature(c, segmentsToDigit.keys.toList), c)).toMap
+
   extension (a: String => Boolean)
     infix def or(b: String => Boolean): String => Boolean = {
       arg => a(arg) || b(arg)
     }
-    
+
   def part1(input: List[String]): Long = {
     val outputs = input.map(i => i.split("\\|")(1)).flatMap(_.split(" "))
     val is1: String => Boolean = _.length == 2
     val is4: String => Boolean = _.length == 4
     val is7: String => Boolean = _.length == 3
     val is8: String => Boolean = _.length == 7
-    
+
     outputs.count(is1 or is4 or is7 or is8)
   }
 
+  case class Line(observations: List[String], output: List[String]) {
+    def correspondanceMap(): Map[Char, Char] = {
+      val observationSignatures = ('a' to 'g').map(segment => (segment, computeSignature(segment, observations))).toMap
+      ('a' to 'g')
+        .map(char => char -> observationSignatures(char))
+        .toMap
+        .view
+        .mapValues(signatureMap.apply)
+        .toMap
+    }
 
+    def solve(): Int = {
+      val mapping = correspondanceMap()
+      val number = output
+        .map(chars => chars.map(mapping.apply).sorted.mkString(""))
+        .map(translatedSegments => segmentsToDigit(translatedSegments).toString)
+        .mkString("")
+      number.toInt
+    }
+  }
+
+  // signature : Map(l -> c) counting the number of segments c of length l in which the segment is present
+  type Signature = Map[Int, Int]
+
+  def computeSignature(segment: Char, observations: List[String]): Signature = {
+    observations
+      .filter(_.contains(segment))
+      .foldLeft(Map.empty[Int, Int]) { case (acc, segments) =>
+        val length = segments.length
+        val updatedCount = acc.getOrElse(segments.length, 0) + 1
+        acc + (length -> updatedCount)
+      }
+  }
+
+  def part2(input: List[String]): Long = {
+    val lines = parseInput(input)
+    lines.map(_.solve()).sum
+  }
+
+
+  def parseInput(input: List[String]): List[Line] = {
+    input.map(i => {
+      val splitted = i.split("\\|")
+      Line(
+        splitted(0).split(" ").map(_.sorted).toList.filter(_.nonEmpty),
+        splitted(1).split(" ").map(_.sorted).toList.filter(_.nonEmpty)
+      )
+    })
+  }
 }
--- src/test/scala/day8/Day8Test.scala
+++ src/test/scala/day8/Day8Test.scala
@@ -4,6 +4,7 @@ package day8
 import day4.Day4.Board
 import day6.Day6.{FishesOfAge, State}
 
+import fr.gplassard.adventofcode2021.day8.Day8.Line
 import org.scalatest.*
 import org.scalatest.matchers.should.*
 import org.scalatest.wordspec.AnyWordSpec
@@ -14,7 +15,6 @@ import scala.jdk.StreamConverters.*
 
 class Day8Test extends AnyWordSpec with Matchers {
 
-
   "part1" should {
     "work for sample" in {
       val inputs = Files.lines(Paths.get("src/test/resources/day8/sample.txt")).toScala(List)
@@ -23,6 +23,125 @@ class Day8Test extends AnyWordSpec with Matchers {
     "work for input" in {
       val inputs = Files.lines(Paths.get("src/test/resources/day8/input.txt")).toScala(List)
       Day8.part1(inputs) should equal(362)
+    }
+  }
+
+  "parseInput" should {
+    "correctly parse the first line" in {
+      val result = Day8.parseInput(List("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"))
+      result should equal(List(Line(
+        List("be", "abcdefg", "bcdefg", "acdefg", "bceg", "cdefg", "abdefg", "bcdef", "abcdf", "bde"),
+        List("abcdefg", "bcdef", "bcdefg", "bceg")
+      )))
+    }
+  }
+
+  "computeSignature" should {
+    "compute the signature of a" in {
+      Day8.computeSignature('a', Day8.segmentsToDigit.keys.toList) should equal(Map(
+        3 -> 1, // a is used 1 time in string of length 3 (7)
+        5 -> 3, // a is used 3 times in string of length 5 (2,3,5)
+        6 -> 3, // 0,6,9
+        7 -> 1, // 8
+      ))
+    }
+    "compute the signature of b" in {
+      Day8.computeSignature('b', Day8.segmentsToDigit.keys.toList) should equal(Map(
+        4 -> 1, // 4
+        5 -> 1, // 5
+        6 -> 3, // 0,6,9
+        7 -> 1, // 8
+      ))
+    }
+    "compute the signature of c" in {
+      Day8.computeSignature('c', Day8.segmentsToDigit.keys.toList) should equal(Map(
+        2 -> 1,
+        3 -> 1,
+        4 -> 1,
+        5 -> 2,
+        6 -> 2,
+        7 -> 1,
+      ))
+    }
+    "compute the signature of d" in {
+      Day8.computeSignature('d', Day8.segmentsToDigit.keys.toList) should equal(Map(
+        4 -> 1,
+        5 -> 3,
+        6 -> 2,
+        7 -> 1,
+      ))
+    }
+    "compute the signature of e" in {
+      Day8.computeSignature('e', Day8.segmentsToDigit.keys.toList) should equal(Map(
+        5 -> 1,
+        6 -> 2,
+        7 -> 1,
+      ))
+    }
+    "compute the signature of f" in {
+      Day8.computeSignature('f', Day8.segmentsToDigit.keys.toList) should equal(Map(
+        2 -> 1,
+        3 -> 1,
+        4 -> 1,
+        5 -> 2,
+        6 -> 3,
+        7 -> 1,
+      ))
+    }
+    "compute the signature of g" in {
+      Day8.computeSignature('g', Day8.segmentsToDigit.keys.toList) should equal(Map(
+        5 -> 3,
+        6 -> 3,
+        7 -> 1,
+      ))
+    }
+    "be different for each segment" in {
+      val signatures = List(
+        Day8.computeSignature('a', Day8.segmentsToDigit.keys.toList),
+        Day8.computeSignature('b', Day8.segmentsToDigit.keys.toList),
+        Day8.computeSignature('c', Day8.segmentsToDigit.keys.toList),
+        Day8.computeSignature('d', Day8.segmentsToDigit.keys.toList),
+        Day8.computeSignature('e', Day8.segmentsToDigit.keys.toList),
+        Day8.computeSignature('f', Day8.segmentsToDigit.keys.toList),
+        Day8.computeSignature('g', Day8.segmentsToDigit.keys.toList),
+      )
+      signatures.toSet.size should equal(7)
+    }
+  }
+
+  "Line" should {
+    "compute the correspondanceMap" in {
+      val line = Line(
+        List("acedgfb","cdfbe","gcdfa","fbcad","dab","cefabd","cdfgeb","eafb","cagedb","ab"),
+        List("cdfeb","fcadb","cdfeb","cdbaf")
+      )
+      line.correspondanceMap() should equal(Map(
+         'd' -> 'a',
+         'e' -> 'b',
+         'a' -> 'c',
+         'f' -> 'd',
+         'g' -> 'e',
+         'b' -> 'f',
+         'c' -> 'g',
+      ))
+    }
+    "solve first line" in {
+      val line = Line(
+        List("be", "abcdefg", "bcdefg", "acdefg", "bceg", "cdefg", "abdefg", "bcdef", "abcdf", "bde"),
+        List("abcdefg", "bcdef", "bcdefg", "bceg")
+      )
+      line.solve() should equal(8394)
+    }
+  }
+
+  "part2" should {
+    "work for sample" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day8/sample.txt")).toScala(List)
+      Day8.part2(inputs) should equal(61229)
+    }
+    "work for input" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day8/input.txt")).toScala(List)
+      Day8.part2(inputs) should equal(1020159)
     }
   }
 
