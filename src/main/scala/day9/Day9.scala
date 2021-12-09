f774b20498f2fdb0b19d0678082cefb90896782b
Gabriel Plassard <gabriel.plassard@gmail.com>
2021-12-09 22:03:07 +0100

    Day 9 / part 2

--- src/main/scala/day9/Day9.scala
+++ src/main/scala/day9/Day9.scala
@@ -5,28 +5,77 @@ import scala.annotation.tailrec
 import scala.util.control.Breaks.*
 
 object Day9 {
-  def isLowPoint(x: Int, y: Int, input: List[String]): Boolean = {
-    val value = input(x).charAt(y)
-    val up = if (x != 0) value < input(x - 1).charAt(y) else true
-    val down = if (x != input.length - 1) value < input(x + 1).charAt(y) else true
-    val left = if (y != 0) value < input(x).charAt(y - 1) else true
-    val right = if (y != input(x).length - 1) value < input(x).charAt(y + 1) else true
-    up && down && left && right
+  type Point = (Int, Int)
+
+  class HeightMap(input: List[String]) {
+    val sizeX: Int = input.length
+    val sizeY: Int = input.head.length
+
+    def valueAt(point: Point): Int = {
+      val (x, y) = point
+      input(x).charAt(y).toString.toInt
+    }
+
+    def neighbors(point: Point): Seq[Point] = {
+      val (x, y) = point
+      val up = if (x != 0) Seq((x - 1, y)) else Seq.empty
+      val down = if (x != sizeX - 1) Seq((x + 1, y)) else Seq.empty
+      val left = if (y != 0) Seq((x, y - 1)) else Seq.empty
+      val right = if (y != sizeY - 1) Seq((x, y + 1)) else Seq.empty
+      up ++ down ++ left ++ right
+    }
+
+    def isLowPoint(point: Point): Boolean = {
+      neighbors(point).forall(neighbor => valueAt(point) < valueAt(neighbor))
+    }
+
+    def findPointsBelongingToBasin(lowpoint: (Int, Int)): Seq[Point] = {
+      var points = Set.empty[Point]
+      var visited = Set.empty[Point]
+      val toVisit = scala.collection.mutable.ListBuffer(lowpoint)
+
+      while (toVisit.nonEmpty) {
+        val element = toVisit.remove(0)
+        val value = input(element._1).charAt(element._2)
+        visited = visited + element
+        points = points + element
+        for (neighbor <- neighbors(element)) {
+          val valid = valueAt(neighbor) != 9 && valueAt(neighbor) > valueAt(element)
+          if (valid && !visited.contains(neighbor)) {
+            toVisit.addOne(neighbor)
+          }
+        }
+      }
+      points.toSeq
+    }
+
   }
 
   def part1(input: List[String]): Long = {
+    val heightMap = new HeightMap(input)
     var risk = 0
     for {
       x <- input.indices
       y <- 0 until input(x).length
     } {
-      val value = input(x).charAt(y)
-      val lowPoint = isLowPoint(x, y, input)
+      val lowPoint = heightMap.isLowPoint((x, y))
       if (lowPoint) {
-        risk += value.toString.toInt + 1
+        risk += heightMap.valueAt(x, y) + 1
       }
     }
     risk
+  }
+
+  def part2(input: List[String]): Long = {
+    val heightMap = new HeightMap(input)
+
+    val basins = for {
+      x <- input.indices
+      y <- 0 until input(x).length
+      if heightMap.isLowPoint((x, y))
+    } yield heightMap.findPointsBelongingToBasin((x, y))
+
+    basins.map(_.length).sorted.reverse.take(3).product
   }
 
 }
--- src/test/scala/day9/Day9Test.scala
+++ src/test/scala/day9/Day9Test.scala
@@ -5,6 +5,7 @@ import day4.Day4.Board
 import day6.Day6.{FishesOfAge, State}
 import day8.Day8.Line
 
+import fr.gplassard.adventofcode2021.day9.Day9.HeightMap
 import org.scalatest.*
 import org.scalatest.matchers.should.*
 import org.scalatest.wordspec.AnyWordSpec
@@ -23,6 +24,44 @@ class Day9Test extends AnyWordSpec with Matchers {
     "work for input" in {
       val inputs = Files.lines(Paths.get("src/test/resources/day9/input.txt")).toScala(List)
       Day9.part1(inputs) should equal(566)
+    }
+  }
+
+  "heightmap.findPointsBelongingToBasin " should {
+    "find the points for the first lowPoint" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day9/sample.txt")).toScala(List)
+      val heightMap = new HeightMap(inputs)
+      heightMap.findPointsBelongingToBasin((0, 1)) contains theSameElementsAs(List(
+        (0, 0),
+        (0, 1),
+        (1, 0)
+      ))
+    }
+    "find the points for the second lowPoint" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day9/sample.txt")).toScala(List)
+      val heightMap = new HeightMap(inputs)
+      heightMap.findPointsBelongingToBasin((0, 9)) contains theSameElementsAs(List(
+        (0, 5),
+        (0, 6),
+        (0, 7),
+        (0, 8),
+        (0, 9),
+        (1, 6),
+        (1, 8),
+        (1, 9),
+        (2, 9),
+      ))
+    }
+  }
+
+  "part2" should {
+    "work for sample" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day9/sample.txt")).toScala(List)
+      Day9.part2(inputs) should equal(1134)
+    }
+    "work for input" in {
+      val inputs = Files.lines(Paths.get("src/test/resources/day9/input.txt")).toScala(List)
+      Day9.part2(inputs) should equal(891684)
     }
   }
 
